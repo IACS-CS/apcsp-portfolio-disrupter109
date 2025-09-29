14a4a2b8b731bbe0a5b9aa2aec5d3aed2f439735
disrupter109 <95176821+disrupter109@users.noreply.github.com>
2025-09-29 12:18:43 +0000

    added brief about me (did back on thursday but never commited)

--- src/scripts/pages/welcome.js
+++ src/scripts/pages/welcome.js
@@ -5,10 +5,10 @@ export function renderWelcome() {
   /* html */
   return `
     <!-- code generated in welcome.js -->
-    <h1>Student Portfolio</h1>
+    <h1>Jayden's Student Portfolio</h1>
     ${renderMenu()}
-    <h2>About Me</h2>
-    <p>All About me...</p>
+    <h2>About Me :)</h2>
+    <p>I am a junior at IACS and a Varsity Captain for the soccer team. I play club soccer for NEFC and town soccer for Dracut. My previous coding experience is literally just my dad teaching me basic Python and when I tried to make a game using Unreal Engine a while back (but I didn't really learn anything).</p>
     <!-- an example image: Notice that images live in public/ -->
     <img src="./example-circle.svg" alt="A blue circle" >
 
