9a9fc2738a16c3ef8d049c2428670f8d8dcd97f1
disrupter109 <95176821+disrupter109@users.noreply.github.com>
2025-10-01 16:27:08 +0000

    day 3: started setting up text and ideas

--- src/scripts/pages/welcome.js
+++ src/scripts/pages/welcome.js
@@ -7,9 +7,11 @@ export function renderWelcome() {
     <!-- code generated in welcome.js -->
     <h1>Jayden's Student Portfolio</h1>
     ${renderMenu()}
-    <h2>About Me :)</h2>
-    <p>I am a junior at IACS and a Varsity Captain for the soccer team. I play club soccer for NEFC and town soccer for Dracut. My previous coding experience is literally just my dad teaching me basic Python and when I tried to make a game using Unreal Engine a while back (but I didn't really learn anything).</p>
-    <!-- an example image: Notice that images live in public/ -->
+    <div class="profile-text">
+      <h2>About Me :)</h2>
+      <p>I am a junior at IACS and a Varsity Captain for the soccer team. I play club soccer for NEFC and town soccer for Dracut. My previous coding experience is literally just my dad teaching me basic Python and when I tried to make a game using Unreal Engine a while back (but I didn't really learn anything).</p>
+    </div>
+      <!-- an example image: Notice that images live in public/ -->
     <img src="./example-circle.svg" alt="A blue circle" >
 
     <!-- end code generated in welcome.js -->
--- src/scripts/utils/renderMenu.js
+++ src/scripts/utils/renderMenu.js
@@ -6,8 +6,12 @@ export function renderMenu() {
     <nav>
       <ul>
         <li><a href="#/">Home</a></li>
+        <div class="profile-text-projects">
         <li><a href="#/binary-search">Binary Search Project</a></li>
+        </div>
+        <div class="profile-text-projects">
         <li><a href="#/citations">Project Citations</a></li>
+        </div>
       </ul>
     </nav>
     <!-- end code generated in renderMenu.js -->
